158ee7b071fd7004097898eb08d2fbd5fe2b8e53
SPRAGE <shauna.pai@gmail.com>
2025-05-25 17:09:01 +0530

    Fix: Comment out clipboard configuration for SSH sessions

--- modules/general/nixvim.nix
+++ modules/general/nixvim.nix
@@ -9,17 +9,10 @@
 
   vimAlias = true; # Enable vim alias to run nvim
 
-  # Ensure clipboard tools are available for SSH sessions
-  extraPackages = with pkgs; [
-    xclip  # X11 clipboard tool
-    xsel   # Alternative X11 clipboard tool
-    wl-clipboard  # Wayland clipboard tool
-  ];
-
   # If using wayland will ensure that wl-copy is enabled
-  clipboard.providers.wl-copy.enable = lib.elem pkgs.system [
-    "x86_64-linux"
-    "aarch64-linux"
-  ];
+  # clipboard.providers.wl-copy.enable = lib.elem pkgs.system [
+  #   "x86_64-linux"
+  #   "aarch64-linux"
+  # ];
 
 }
